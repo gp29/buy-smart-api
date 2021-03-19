43fb314e72d129e9834e1493c3364157f1e5c7c1
Gaurav Patel <gaurav.gkp29@gmail.com>
2021-03-19 12:27:23 +0530

    CHanges

--- app.js
+++ app.js
@@ -38,8 +38,8 @@ const multipartyMiddleWare = multiparty();
 const app = express();
 app.use(favicon(path.join(__dirname, './public/img', 'favicon.ico')));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(bodyParser.json({limit: '50mb'}));
-app.use(bodyParser.urlencoded({extended: true, limit: '50mb', parameterLimit: 1000000}));
+app.use(bodyParser.json({limit: '100mb'}));
+app.use(bodyParser.urlencoded({extended: true, limit: '100mb', parameterLimit: 1000000}));
 app.use(cookieParser());
 app.use(require('express-session')({
     secret: 'keyboard cat',

c6c04a3e6e2168b8d2f9e6796c47b0f72b2a35ce
Gaurav Patel <gaurav.gkp29@gmail.com>
2021-04-07 23:02:29 +0530

    changes

--- model_handlers/api/product-handler.js
+++ model_handlers/api/product-handler.js
@@ -42,7 +42,6 @@ const getResults = async(requestParam, code) => {
                 }
                 callbackSingleRec();
             }, async function(){
-                console.log(indexArr)
                 let products = await query.selectWithAnd(dbConstants.dbSchema.products, {Index: {$in: indexArr}}, { _id: 0, created_at:0, updated_at:0, __v:0} );
                 await query.updateMultiple(dbConstants.dbSchema.products, { $inc: { Query_count: 1 } }, { Index: {$in: indexArr} });
                 insertResultData(products);
@@ -134,9 +133,63 @@ const dataInsert = async(singleRec, code) => {
                 return;
             }
             else{
-                console.log("UPDATE RECORD")
-                await query.updateSingle(dbConstants.dbSchema.products, singleRec, { Index: parseFloat(res) });
-                await query.updateMultiple(dbConstants.dbSchema.results, singleRec, { Index: parseFloat(res) });
+                let updateObj = {};
+                if(singleRec.category_id && (singleRec.category_id!='' || singleRec.category_id!='null' || singleRec.category_id!='NULL')){
+                    updateObj.category_id = singleRec.category_id
+                }
+                if(singleRec.Host && (singleRec.Host!='' || singleRec.Host!='null' || singleRec.Host!='NULL')){
+                    updateObj.Host = singleRec.Host
+                }
+                if(singleRec.Img_key && (singleRec.Img_key!='' || singleRec.Img_key!='null' || singleRec.Img_key!='NULL')){
+                    updateObj.Img_key = singleRec.Img_key
+                }
+                if(singleRec.Product_key && (singleRec.Product_key!='' || singleRec.Product_key!='null' || singleRec.Product_key!='NULL')){
+                    updateObj.Product_key = singleRec.Product_key
+                }
+                if(singleRec.Brand_name && (singleRec.Brand_name!='' || singleRec.Brand_name!='null' || singleRec.Brand_name!='NULL')){
+                    updateObj.Brand_name = singleRec.Brand_name
+                }
+                if(singleRec.Product_Title && (singleRec.Product_Title!='' || singleRec.Product_Title!='null' || singleRec.Product_Title!='NULL')){
+                    updateObj.Product_Title = singleRec.Product_Title
+                }
+                if(singleRec.Product_Url && (singleRec.Product_Url!='' || singleRec.Product_Url!='null' || singleRec.Product_Url!='NULL')){
+                    updateObj.Product_Url = singleRec.Product_Url
+                }
+                if(singleRec.Img_url && (singleRec.Img_url!='' || singleRec.Img_url!='null' || singleRec.Img_url!='NULL')){
+                    updateObj.Img_url = singleRec.Img_url
+                }
+                if(singleRec.Mrp && (singleRec.Mrp!='' || singleRec.Mrp!='null' || singleRec.Mrp!='NULL')){
+                    updateObj.Mrp = singleRec.Mrp
+                }
+                if(singleRec.SellPrice && (singleRec.SellPrice!='' || singleRec.SellPrice!='null' || singleRec.SellPrice!='NULL')){
+                    updateObj.SellPrice = singleRec.SellPrice
+                }
+                if(singleRec.Discount && (singleRec.Discount!='' || singleRec.Discount!='null' || singleRec.Discount!='NULL')){
+                    updateObj.Discount = singleRec.Discount
+                }
+                if(singleRec.Colour && (singleRec.Colour!='' || singleRec.Colour!='null' || singleRec.Colour!='NULL')){
+                    updateObj.Colour = singleRec.Colour
+                }
+                if(singleRec.Rating_Count && (singleRec.Rating_Count!='' || singleRec.Rating_Count!='null' || singleRec.Rating_Count!='NULL')){
+                    updateObj.Rating_Count = singleRec.Rating_Count
+                }
+                if(singleRec.Review_Count && (singleRec.Review_Count!='' || singleRec.Review_Count!='null' || singleRec.Review_Count!='NULL')){
+                    updateObj.Review_Count = singleRec.Review_Count
+                }
+                if(singleRec.Rating_Star && (singleRec.Rating_Star!='' || singleRec.Rating_Star!='null' || singleRec.Rating_Star!='NULL')){
+                    updateObj.Rating_Star = singleRec.Rating_Star
+                }
+                if(singleRec.Time_stamp){
+                    updateObj.Time_stamp = new Date()
+                }
+                if(singleRec.SS_update){
+                    updateObj['$inc'] = { SS_update: 1 }
+                }
+                if(singleRec.CS_update){
+                    updateObj['$inc'] = { CS_update: 1 }
+                }
+                await query.updateSingle(dbConstants.dbSchema.products, updateObj, { Index: parseFloat(res) });
+                await query.updateMultiple(dbConstants.dbSchema.results, updateObj, { Index: parseFloat(res) });
                 resolve({});
                 return;
             }
@@ -151,7 +204,6 @@ const dataInsert = async(singleRec, code) => {
 const dataInsertPost = async(requestParam, code) => {
     return new Promise(async(resolve, reject) => {
         try {
-            console.log(requestParam.data.length)
             asyncLoop.forEachSeries(requestParam.data, async function(singleRec, callbackSingleRec) {
                 let res = await dataInsert(singleRec);
                 callbackSingleRec();
@@ -171,7 +223,6 @@ const dataInsertAndroid = async(requestParam, code) => {
     return new Promise(async(resolve, reject) => {
         try {
             requestParam.data = JSON.parse(requestParam.data)
-            console.log(requestParam.data.length)
             asyncLoop.forEachSeries(requestParam.data, async function(singleRec, callbackSingleRec) {
                 let res = await dataInsert(singleRec);
                 callbackSingleRec();
@@ -270,7 +321,7 @@ const feed = async(requestParam, code) => {
             let feeds = await query.selectWithAnd(dbConstants.dbSchema.feeds, {}, { _id: 0, Index:1} );
             arr = await query.selectWithAnd(dbConstants.dbSchema.products, {Index: {$in: _.pluck(feeds, 'Index')}}, { _id: 0, created_at:0, updated_at:0, __v:0} );
 
-            let limit = 15;
+            let limit = 20;
             let page = parseFloat(requestParam.page) - 1;
             let skip = page * limit;
             let data = await query.selectWithAndFilter(dbConstants.dbSchema.products, {Index: {$nin: _.pluck(feeds, 'Index')}}, {

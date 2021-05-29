9da3c6b857badb277b65352aa617ca8529b45b31
Gaurav Patel <gaurav.gkp29@gmail.com>
2021-05-29 16:10:01 +0530

    changes

--- model_handlers/backend/product-handler.js
+++ model_handlers/backend/product-handler.js
@@ -144,7 +144,7 @@ const getProductKeyExtract = async (url) => {
         try {
             let finalString;
             if(url.includes('amazon.in') == true){
-                finalString = "AM" + url.split('/')[5]
+                finalString = "AM" + url.split('/')[4]
             }
             else if(url.includes('flipkart.com') == true){
                 if(url.split('/')[6] == undefined){
